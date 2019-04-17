6c51e79047a4529bed69e9a7530cc294dd8dbe69
huong9x <huongnv@sphinx-software.com>
2019-04-18 01:39:36 +0700

    a temp test version

--- app.js
+++ app.js
@@ -6,35 +6,20 @@ const Division    = require('./Operator/division');
 const chai        = require('chai');
 const sinon       = require('sinon');
 
-class FakeOperator {
-    constructor(shouldReturn) {
-        this.calles = [];
-        this.shouldReturn = shouldReturn;
-    }
-    callCount() {
-        return this.calles.length;
-    }
-    getCalled(index) {
-        return this.calles[index];
-    }
-    run(number1, number2) {
-        this.calles.push([number1, number2]);
-        return this.shouldReturn;
-    }
-}
 
 describe('Test calculator with addition', () => {
     const calculator = new Calculator();
-    const fakeOps    = new FakeOperator(2019);
+    const fakeOps    = {
+        run(number1, number2) {
+            return sinon.fake.returns(2019);
+        }
+    }
     calculator.register('fake', fakeOps);
 
     it('should return result from operator', () => {
         const result = calculator.calculate('fake', 1, 2);
-        chai.assert.equal(result, 2019);
-        chai.assert.equal(fakeOps.callCount(), 1);
-
-        const callParameters = fakeOps.getCalled(0);
-        chai.assert.equal(callParameters[0], 1);
-        chai.assert.equal(callParameters[1], 2);
+        sinon.assert.equals(calculator.calculate('fake', 1 , 2), 2019);
+        sinon.assert.calledOnce(fakeOps.run(1, 2));
+        sinon.assert(fakeOps.run().calledWith(1, 2));
     });
 });
